551ba0cde0eb7c6393ef8ea504f69a7ecb889e7d
Ramneek Singh <ramneek.singh@csiro.au>
2018-01-11 11:34:53 +1100

    sols

--- Programming with R for datasc/RProject1/script2.R
+++ Programming with R for datasc/RProject1/script2.R
@@ -759,5 +759,35 @@ ncol(my.data)
 data1<-my.data$systolic.bp[my.data$Genotype=="BA"] 
 data2<-my.data$systolic.bp[my.data$Genotype=="BB"] 
 
+testResult <- t.test(data1,data2)
 
-t.test()
+plot(density(data1))
+lines(density(data2))
+
+n1 <- length(my.data[my.data$Genotype=="BA",c("Genotype")])
+
+testResult <- t.test(runif(26,min=1, max=2),data2)
+
+set.seed(1234)
+my.new.data<-my.data
+my.new.data$Genotype<-"BB"
+index.temp<-sample(1:50,n1)  
+my.new.data$Genotype[index.temp]<-"BA"
+new.data1<-my.new.data$systolic.bp[my.new.data$Genotype=="BA"]
+new.data2<-my.new.data$systolic.bp[my.new.data$Genotype=="BB"]
+t.test(new.data1,new.data2)$statistic
+
+
+
+my.new.data<-my.data
+my.new.data$Genotype<-"BB"
+doone<-function(){
+  index.temp<-sample(1:50,n1)  
+  my.new.data$Genotype[index.temp]<-"BA"
+  new.data1<-my.new.data$systolic.bp[my.new.data$Genotype=="BA"]
+  new.data2<-my.new.data$systolic.bp[my.new.data$Genotype=="BB"]
+  return(t.test(new.data1,new.data2)$statistic)
+}
+set.seed(554)
+my.t.values<-replicate(100000,doone())
+mean((1*(my.t.values)^2>2.027021^2))
