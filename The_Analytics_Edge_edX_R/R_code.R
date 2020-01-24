cfd899067baf97f15f003bdf01a2d806653cc65e
Ramneek Singh <ramneek.singh@csiro.au>
2020-01-24 15:17:50 +1100

    update

--- The_Analytics_Edge_edX_R/R_code.R
+++ The_Analytics_Edge_edX_R/R_code.R
@@ -4,6 +4,11 @@ getwd()
 setwd("C:/Users/sin17h/Downloads")
 ls()
 
+#ggplots drag and drop plot builder
+#esquisse::esquisser()
+#tidylog
+#heatmaply
+
 w = read.csv('who.csv')
 str(w)
 mn = mean(w$Over60)
@@ -232,3 +237,101 @@ str(model1);
 summary(model1)
 
 model2 = lm(Price ~ AGST + HarvestRain, data=wine)
+summary(model2)
+
+ml1 = lm(Price ~ HarvestRain + WinterRain, data=wine)
+summary(ml1)
+
+cor(wine)
+cor(wine$Price, wine$Year)
+table(wine$Price, wine$Year)
+plot(wine$Price, wine$Year)
+plot(wine$Price, wine$Year, type="l", col='red')
+
+cor(wine$HarvestRain, wine$WinterRain)
+
+bball = read.csv("baseball.csv")
+mball = subset(bball, Year <2002)
+mball$RD = mball$RS - mball$RA
+winsReg = lm(W~RD, data=mball)
+wins = 80.8814 + 99*.1058
+
+# VIDEO 1
+
+# Read in the data
+NBA = read.csv("NBA_train.csv")
+str(NBA)
+
+
+# VIDEO 2
+
+# How many wins to make the playoffs?
+table(NBA$W, NBA$Playoffs)
+
+# Compute Points Difference
+NBA$PTSdiff = NBA$PTS - NBA$oppPTS
+
+# Check for linear relationship
+plot(NBA$PTSdiff, NBA$W)
+
+# Linear regression model for wins
+WinsReg = lm(W ~ PTSdiff, data=NBA)
+summary(WinsReg)
+
+
+# VIDEO 3
+
+# Linear regression model for points scored
+PointsReg = lm(PTS ~ X2PA + X3PA + FTA + AST + ORB + DRB + TOV + STL + BLK, data=NBA)
+summary(PointsReg)
+
+# Sum of Squared Errors
+PointsReg$residuals
+SSE = sum(PointsReg$residuals^2)
+SSE
+
+# Root mean squared error
+RMSE = sqrt(SSE/nrow(NBA))
+RMSE
+
+# Average number of points in a season
+mean(NBA$PTS)
+
+# Remove insignifcant variables
+summary(PointsReg)
+
+PointsReg2 = lm(PTS ~ X2PA + X3PA + FTA + AST + ORB + DRB + STL + BLK, data=NBA)
+summary(PointsReg2)
+
+PointsReg3 = lm(PTS ~ X2PA + X3PA + FTA + AST + ORB + STL + BLK, data=NBA)
+summary(PointsReg3)
+
+PointsReg4 = lm(PTS ~ X2PA + X3PA + FTA + AST + ORB + STL, data=NBA)
+summary(PointsReg4)
+
+# Compute SSE and RMSE for new model
+SSE_4 = sum(PointsReg4$residuals^2)
+RMSE_4 = sqrt(SSE_4/nrow(NBA))
+SSE_4
+RMSE_4
+
+
+
+
+# VIDEO 4
+
+# Read in test set
+NBA_test = read.csv("NBA_test.csv")
+
+# Make predictions on test set
+PointsPredictions = predict(PointsReg4, newdata=NBA_test)
+
+# Compute out-of-sample R^2
+SSE = sum((PointsPredictions - NBA_test$PTS)^2)
+SST = sum((mean(NBA$PTS) - NBA_test$PTS)^2)
+R2 = 1 - SSE/SST
+R2
+
+# Compute the RMSE
+RMSE = sqrt(SSE/nrow(NBA_test))
+RMSE
