b06edfc80a415f643e8bf6e8f1eb9e01acc9c3dc
Ramneek Singh <ramneek.singh@csiro.au>
2020-01-24 15:45:50 +1100

    new read

---
+++ ISLR Seventh Printing/R_code.R
@@ -0,0 +1,12 @@
+Sys.getlocale()
+# Sys.setlocale("LC_ALL", "C")
+getwd()
+setwd("C:/Users/sin17h/Downloads")
+ls()
+
+#ggplots drag and drop plot builder
+#esquisse::esquisser()
+#tidylog
+#heatmaply
+
+w = read.csv('who.csv')
