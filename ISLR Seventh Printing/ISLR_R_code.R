fa44107d2230f6c9aa0212ad2a3fb01bf3b379f2
Ramneek Singh <ramneek.singh@csiro.au>
2020-02-19 18:11:06 +1100

    update

--- ISLR Seventh Printing/ISLR_R_code.R
+++ ISLR Seventh Printing/ISLR_R_code.R
@@ -448,8 +448,111 @@ table(glm.pred ,Direction)#confidence matrix
 mean(glm.pred == Direction)#fraction of days for which prediction was correct. Same as calculated above from confusion matrix.
 
 train =(Year <2005)
-Smarket.2005= ISLR::Smarket[! train ,] # can be done using subset as well 
+Smarket.2005= ISLR::Smarket[!train,]#can be done using subset as well 
 dim(subset(sm, !Year<2005))
 dim(Smarket.2005)
 Direction.2005= Direction[!train]
 
+glm.fits=glm(Direction~Lag1+Lag2+Lag3+Lag4+Lag5+Volume ,
+               data=sm ,family =binomial ,subset =train )
+glm.probs =predict(glm.fits,Smarket.2005 , type="response")
+
+glm.pred = rep("Down", 252)
+glm.pred[glm.probs>.5] = "Up"
+table(glm.pred,Direction.2005)#confusion matrix to calculate the error rate. Correct prediction rate in this case is (507+145)/1250.
+mean(glm.pred == Direction.2005)#this can also be used to do same
+mean(glm.pred != Direction.2005)#error rate is .519 which is worse than random guesses
+
+#retrain the model on the training subset on only the predictors which have a low value(hence more significant). But why is Volume not included? 
+glm.fits=glm(Direction~Lag1+Lag2, data=ISLR::Smarket, family=binomial, subset = train)
+summary(glm.fits)
+
+#now predict
+glm.probs = predict(glm.fits, Smarket.2005, type="response")
+glm.pred = rep("Down", 252)
+glm.pred[glm.probs>.5] = "Up"
+table(glm.pred,Direction.2005)
+mean(glm.pred == Direction.2005)# Correct prediction rate has increased. it is now 56%
+
+predict (glm.fits,newdata =data.frame(Lag1=c(1.2 ,1.5),Lag2=c(1.1 , -0.8) ),type ="response")
+
+
+#linear discriminant analysis
+library(MASS)
+lda.fit=lda(Direction~Lag1+Lag2 ,data=ISLR::Smarket ,subset =train)
+lda.fit
+summary(lda.fit)
+plot(lda.fit)
+
+#predict
+lda.pred=predict (lda.fit , Smarket.2005)
+names(lda.pred)
+
+lda.class = lda.pred$class
+table(lda.class,Direction.2005)
+mean(lda.class == Direction.2005)#correct prediction rate
+
+sum(lda.pred$posterior [ ,1] >=.5)
+sum(lda.pred$posterior [,1]<.5)
+
+lda.pred$posterior[1:15 ,1]
+lda.class[1:15]
+
+#quadratic disciminant analysis
+qda.fit=qda(Direction~Lag1+Lag2, data=ISLR::Smarket, subset=train)
+qda.fit
+
+qda.class =predict(qda.fit, Smarket.2005)$class
+table(qda.class ,Direction.2005)
+
+#now predict
+qda.class = predict (qda.fit, Smarket.2005)$class
+table(qda.class, Direction.2005)
+mean(qda.class == Direction.2005)#this quadratic correct prediction rate is greater than linear LDA and Logistic regression.
+
+#knn (k nearest neighbours) classification
+train =(Year <2005)
+library (class)
+train.X=cbind(Lag1 ,Lag2)[train ,]
+test.X=cbind (Lag1 ,Lag2)[!train ,]
+train.Direction =Direction [train]
+
+#predict
+set.seed(1)
+knn.pred=knn(train.X,test.X,train.Direction ,k=1)#k=1 the number of nearest neighbours to be used by the classifier
+table(knn.pred ,Direction.2005)
+mean(knn.pred == Direction.2005)#only 50% correct prediction rate
+
+knn.pred=knn(train.X,test.X,train.Direction, k=3)#perhaps increasing k to 3 might improve prediction rate
+table(knn.pred ,Direction.2005)
+mean(knn.pred == Direction.2005)#slight improvement in correct prediction rate.
+
+#in the end, for this classification excecise, out of lda,knn,qda, logistic regression, it is the qda which performs the best
+
+Caravan = ISLR::Caravan
+dim(Caravan)
+attach(Caravan)
+summary(Caravan$Purchase)
+
+#scale or standardize the data. There is a subtle difference between them but standarization is preferred (sd of 1 and mean of 0).
+standardized.X=scale(Caravan[,-86])
+var(Caravan[,1])
+var(standardized.X[,1])
+
+#now predict using standardized data.
+test =1:1000
+train.X=standardized.X[-test ,]
+test.X=standardized.X[test ,]
+train.Y=Purchase [-test]
+test.Y=Purchase [test]
+set.seed (1)
+knn.pred=knn(train.X,test.X,train.Y,k=1)
+mean(test.Y!= knn.pred)
+mean(test.Y!=" No")
+
+knn.pred=knn(train.X,test.X,train.Y,k=3)
+table(knn.pred ,test.Y)
+
+knn.pred=knn(train.X,test.X,train.Y,k=5)
+table(knn.pred ,test.Y)
+#page 167 bookmark
